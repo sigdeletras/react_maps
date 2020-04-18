7a4791fcc21ea0322fa93781a23e643b7d26519e
Patricio Soriano <pasoriano@gmail.com>
2020-04-18 09:27:28 +0200

    02 grid boostrap

--- geoapp/src/App.js
+++ geoapp/src/App.js
@@ -1,11 +1,65 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-     <h1>GeoApp</h1>
-    </div>
-  );
-}
+import './App.css';
 
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row m-3">
+          <div className="col-12">
+            <h1 className="text-center">GeoApp</h1>
+          </div>
+        </div>
+        <div className="row m-3">
+          <div className="col-sm-4 col-md-2">
+
+            <div class="form-group">
+              <label for="FormControlSelect1">Municipality</label>
+              <select class="form-control" id="eFormControlSelect1">
+                <option>Montoro</option>
+                <option>Lucena</option>
+
+              </select>
+            </div>
+            <button className="btn btn-primary mb-3">Load</button>
+          </div>
+          <div id="map" className="col-sm-8 col-sm-offset-4 col-md-10 col-md-offset-3">
+          </div>
+        </div>
+        <div className="row m-3">
+          <div className="col">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Type</th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Type 1</td>
+                  <td>name 1</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Type 2</td>
+                  <td>name 2</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Type 1</td>
+                  <td>name 3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
--- geoapp/src/index.js
+++ geoapp/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
+
 import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
-
-
