44c5743ab7755658435c29665021127da5b79d57
Patricio Soriano <pasoriano@gmail.com>
2020-04-19 08:30:38 +0200

    Datos de index.html Title

--- geoapp/src/index.js
+++ geoapp/src/index.js
@@ -8,11 +8,7 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
-    document.getElementById('root')
+ReactDOM.render(<React.StrictMode >
+    <App/>
+    </React.StrictMode>,document.getElementById('root')
 );
