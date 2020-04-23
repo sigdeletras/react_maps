3b0484837140e4a432fdf2a2616d65cce68f61e6
Patricio Soriano <pasoriano@gmail.com>
2020-04-23 12:08:47 +0200

    selectCoord a state, función selectListChange, y button y funcioón con  disabled

--- geoapp/src/components/SelectList/SelectList.js
+++ geoapp/src/components/SelectList/SelectList.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import municipalities from './data/ municipalities.json'
 
-class SelectList extends React.Component {
+export default class SelectList extends React.Component {
 
     state = {
-        data: municipalities
+        data: municipalities,
+        selectCoord: '',
+    }
+
+    selectListChange = (e) => {
+        if (e.target.value) {
+            this.setState({
+                selectCoord: e.target.value,
+            });
+        }
     }
 
     render() {
@@ -12,16 +21,17 @@ class SelectList extends React.Component {
         return (
             <div>
                 <label>Municipality</label>
-                <select
-                    className="form-control"
-                    onChange={this.selectListChange}>
-                    <option value="">Selecciona un municipio</option>
+                <select className="form-control" onChange={this.selectListChange}>
+                    <option value="">Select one...</option>
                     {data.map((m) =>
                         <option key={m.id} value={[m.coordinates, m.name]}>{m.name}</option>
                     )}
                 </select>
+                <button className="btn btn-primary mt-3 mb-3"
+                    disabled={!this.state.selectCoord}
+                    onClick={this.props.munipalityChange.bind(this, this.state.selectCoord)}>
+                    Load</button>
             </div>
         )
     }
 }
-export default SelectList;
