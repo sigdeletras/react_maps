b1931fcdbb3f8194c3b59ac3a385bfc242c83cdb
Patricio Soriano <pasoriano@gmail.com>
2020-04-27 20:49:35 +0200

    ref y métodos de ciclo de vida en maplayer

--- geoapp/src/components/MapLayer/MapLayer.js
+++ geoapp/src/components/MapLayer/MapLayer.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import datamontoro from "./data/montoro.json"
 import { GeoJSON } from 'react-leaflet';
 import L from 'leaflet';
 
 delete L.Icon.Default.prototype._getIconUrl;
-
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
     iconUrl: require('leaflet/dist/images/marker-icon.png'),
@@ -13,45 +11,34 @@ L.Icon.Default.mergeOptions({
 
 export default class MapLayer extends Component {
 
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //         key: this.props.layer.name,
-    //         data: this.props.layer,
-    //         style: {
-    //             weight: 0,
-    //             fillColor: this.props.color,
-    //             fillOpacity: this.props.color,
-    //             styleChanged: false
-    //         },
-    //     }
-    // }
+    leafletRef = React.createRef();
 
+    componentWillReceiveProps(prevProps) {
+        if (prevProps.data !== this.props.data) {
+            this.leafletRef.current.leafletElement.clearLayers();
+        }
+    }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.data !== this.props.data) {
+            this.leafletRef.current.leafletElement.addData(this.props.data);
+        }
+    }
 
-  // geoJSONStyle() {
-  //   return {
-  //     color: '#1f2021',
-  //     weight: 1,
-  //     fillOpacity: 0.5,
-  //     fillColor: '#fff2af',
-  //   }
-  // }
+    onEachFeature(feature, layer) {
+        const popupContent = `<Popup>
+                                    <pre>Centro: ${feature.properties.centro}</pre>
+                                    <pre>Tipo: ${feature.properties.tipocentro}</pre>
+                                    <pre>Titularidad: ${feature.properties.titularida}</pre>
+                                </Popup>`
+        layer.bindPopup(popupContent)
+    }
 
-  onEachFeature(feature, layer) {
-    const popupContent = ` <Popup><p>Customizable Popups <br />with feature information.</p><pre>Borough: <br />${feature.properties.name}</pre></Popup>`
-    layer.bindPopup(popupContent)
-  }
-    
     render() {
-        return (
-            <div>
-                <GeoJSON
-                    // key={this.state.key}
-                    data={datamontoro}
-                    onEachFeature={this.onEachFeature} 
-                />
-            </div>
-        )
+        return <GeoJSON
+        data = { this.props.data }
+        onEachFeature = { this.onEachFeature }
+        ref = { this.leafletRef }
+        />
     }
 }
